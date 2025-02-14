2d84bfde3930d19929cd11ce926335372cd4824e
albertolovell <alberto.lovell@gmail.com>
2025-02-14 13:47:27 -0800

    k6 load test 1kvus

---
+++ tests/load_test.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '30s', target: 100 },
+    { duration: '1m', target: 1000 },
+    { duration: '1m', target: 0 }
+  ]
+};
+
+export default function () {
+  let res = http.get('http://localhost:3000/products');
+  check(res, {
+    'status is 200': (r) => r.status === 200
+  });
+  sleep(1);
+};
