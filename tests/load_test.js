84d3f9c6d53a2350954e58571fb7beaef9ca774e
albertolovell <alberto.lovell@gmail.com>
2025-02-14 23:54:15 -0800

    k6 cloud

--- tests/load_test.js
+++ tests/load_test.js
@@ -1,18 +1,28 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-export let options = {
+export const options = {
   stages: [
-    { duration: '30s', target: 100 },
-    { duration: '1m', target: 1000 },
-    { duration: '1m', target: 0 }
-  ]
+    { duration: '30s', target: 10 },
+    { duration: '1m', target: 10 },
+    { duration: '30s', target: 0 }
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.02'],
+    http_req_duration: ['p(95)<2000'],
+  },
+  cloud: {
+    distribution: {
+      'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 },
+    },
+  },
 };
 
 export default function () {
-  let res = http.get('http://localhost:3000/products');
+  let res = http.get('http://localhost:3000/products/40344/styles');
   check(res, {
     'status is 200': (r) => r.status === 200
   });
   sleep(1);
 };
+
