5cc6e68b38d69bbafed342d0fb1f2ba9d0cd6d15
albertolovell <alberto.lovell@gmail.com>
2025-02-16 16:29:00 -0800

    denormalized schema products + styles

--- server/db.js
+++ server/db.js
@@ -1,25 +1,16 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-// before refactor
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('error', (err) => console.error('mongoose failed: ', err));
 
-const featureSchema = mongoose.Schema ({
-  id: { type: Number, unique: true, index: true },
-  product_id: { type: Number, ref: 'Product', required: true, index: true },
+const featuresSchema = mongoose.Schema ({
   feature: { type: String },
   value: { type: String }
-});
-
-const relatedSchema = mongoose.Schema ({
-  id: { type: Number, unique: true, index: true },
-  product_id: { type: Number, ref: 'Product', index: true },
-  related_product_id: { type: Number, ref: 'Product', required: true, index: true }
-});
+})
 
 const productSchema = mongoose.Schema ({
-  id: { type: Number, unique: true, required: true, index: true },
+  id: { type: Number, unique: true },
   campus: { type: String },
   name: { type: String },
   slogan: { type: String },
@@ -27,49 +18,39 @@ const productSchema = mongoose.Schema ({
   category: { type: String },
   default_price: { type: String },
   created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
+  updated_at: { type: Date, default: Date.now },
+  features: [featuresSchema]
 });
 
 
-const photoSchema = mongoose.Schema ({
-  id: { type: Number, unique: true, index: true },
-  style_id: { type: Number, ref: 'Style', required: true, index: true },
+const photosSchema = mongoose.Schema ({
   thumbnail_url: { type: String },
   url: { type: String }
-});
+})
 
-const skuSchema = mongoose.Schema ({
-  id: {type: Number, unique: true, index: true },
-  style_id: { type: Number, ref: 'Style', required: true, index: true },
+const skusSchema = mongoose.Schema ({
   quantity: {type: Number},
   size: {type: String}
-});
+})
 
 const styleSchema = mongoose.Schema ({
-  product_id: { type: Number, ref: 'Product', required: true, index: true },
-  style_id: { type: Number, index: true },
-  name: { type: String },
-  original_price: { type: String },
-  sale_price: { type: String },
-  default_style: { type: Boolean, default: false }
+  product_id: { type: Schema.Types.ObjectId, ref: 'Product' },
+  results: [
+    {
+      style_id: { type: Number, unique: true },
+      name: { type: String },
+      original_price: { type: String },
+      sale_price: { type: String },
+      default?: { type: Boolean, default: false },
+      photos: [photosSchema],
+      skus: {
+        type: Map,
+        of: skusSchema
+      }
+    }
+  ]
 });
 
 
-const cartSchema = mongoose.Schema ({
-  id: { type: Number, unique: true, required: true, index: true },
-  user_session: { type: Number, index: true },
-  sku_id: { type: Number, index: true },
-  count: { type: String }
-});
-
-
-const Feature = mongoose.model('Feature', featureSchema);
-const Related = mongoose.model('Related', relatedSchema);
 const Product = mongoose.model('Product', productSchema);
-const Photo = mongoose.model('Photo', photoSchema);
-const Sku = mongoose.model('Sku', skuSchema);
 const Style = mongoose.model('Style', styleSchema);
-const Cart = mongoose.model('Cart', cartSchema);
-
-module.exports = { Feature, Related, Product, Photo, Sku, Style, Cart };
-
