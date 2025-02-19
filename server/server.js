2a8ebdadb28def2c78cce686de3b27cfb8713386
albertolovell <alberto.lovell@gmail.com>
2025-02-19 10:37:45 -0800

    added node clustering

--- server/server.js
+++ server/server.js
@@ -3,88 +3,109 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const { getStyles, getProduct, getProductView, getRelated } = require('./model.js');
-// deploy
 
-const app = express();
+const cluster = require('cluster');
+const os = require('os');
+const numCPUs = os.cpus().length;
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+if (cluster.isMaster) {
+  console.log(`Master process ${process.pid} is running`);
 
-app.get('/loaderio-168a9700e107f67eb4e7572498834a5f.txt', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'loaderio-168a9700e107f67eb4e7572498834a5f.txt'));
-});
-
-app.get('/products', async (req, res) => {
-  try {
-    const page = parseInt(req.query.page) || 1;
-    const count = parseInt(req.query.count) || 5;
-
-    const products = await getProductView(page, count);
-
-    return res.status(200).json(products);
-  } catch (err) {
-    console.error('products view failed: ', err);
-    return res.status(500);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-});
 
-app.get('/products/:id', async (req, res) => {
-  try {
-    const productId = parseInt(req.params.id);
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker process ${worker.process.pid} died. Restarting...`);
+    cluster.fork();
+  });
+} else {
 
-    const product = await getProduct(productId)
+  const app = express();
 
-    return res.status(200).json(product);
-  } catch (err) {
-    console.error('product fetch failed: ', err);
-    return res.status(500);
-  }
-});
+  app.use(express.json());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/products/:id/styles', async (req, res) => {
-  try {
-    const productId = parseInt(req.params.id);
+  app.get('/loaderio-168a9700e107f67eb4e7572498834a5f.txt', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'loaderio-168a9700e107f67eb4e7572498834a5f.txt'));
+  });
 
-    const styles = await getStyles(productId);
+  app.get('/products', async (req, res) => {
+    try {
+      const page = parseInt(req.query.page) || 1;
+      const count = parseInt(req.query.count) || 5;
 
-    return res.status(200).json(styles);
-  } catch (err) {
-    console.error('styles fetch failed: ', err);
-    return res.status(500);
-  }
-});
+      const products = await getProductView(page, count);
 
-app.get('/products/:id/related', async (req, res) => {
-  try {
-    const productId = parseInt(req.params.id);
+      return res.status(200).json(products);
+    } catch (err) {
+      console.error('products view failed: ', err);
+      return res.status(500);
+    }
+  });
 
-    const related = await getRelated(productId);
+  app.get('/products/:id', async (req, res) => {
+    try {
+      const productId = parseInt(req.params.id);
 
-    return res.status(200).json(related.related);
-  } catch (err) {
-    console.error('related fetch failed: ', err);
-    return res.status(500);
-  }
-});
+      const product = await getProduct(productId)
 
-app.get('/cart', async (req, res) => {
-  try {
-    // TODO: implement authentication
+      return res.status(200).json(product);
+    } catch (err) {
+      console.error('product fetch failed: ', err);
+      return res.status(500);
+    }
+  });
+
+  app.get('/products/:id/styles', async (req, res) => {
+    try {
+      const productId = parseInt(req.params.id);
+
+      const styles = await getStyles(productId);
+
+      return res.status(200).json(styles);
+    } catch (err) {
+      console.error('styles fetch failed: ', err);
+      return res.status(500);
+    }
+  });
+
+  app.get('/products/:id/related', async (req, res) => {
+    try {
+      const productId = parseInt(req.params.id);
+
+      const related = await getRelated(productId);
+
+      return res.status(200).json(related.related);
+    } catch (err) {
+      console.error('related fetch failed: ', err);
+      return res.status(500);
+    }
+  });
+
+  app.get('/cart', async (req, res) => {
+    try {
+      // TODO: implement authentication
+      res.status(200).json('Endpoint setup in process...');
+    } catch (err) {
+      console.error('cart fetch failed: ', err);
+      return res.status(500);
+    }
+  });
+
+  app.post('/cart', async (req, res) => {
     res.status(200).json('Endpoint setup in process...');
-  } catch (err) {
-    console.error('cart fetch failed: ', err);
-    return res.status(500);
-  }
-});
+  });
 
-app.post('/cart', async (req, res) => {
-  res.status(200).json('Endpoint setup in process...');
-});
+  app.listen(process.env.PORT); // 3000
+  console.log(`Worker process ${process.pid} listening at http://54.160.237.234:${process.env.PORT}`);
 
-app.listen(process.env.PORT); // 3000
-console.log(`Listening at http://54.160.237.234:${process.env.PORT}`);
+  module.exports = app;
 
-module.exports = app;
+}
+
+
+
 
 
 
